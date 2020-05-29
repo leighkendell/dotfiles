34a6bc5085530a941719b0742ca3a8287ef127f9
Leigh Kendell <leighkendell@gmail.com>
2020-05-29 16:58:49 +0800

    Create .hyper.js

---
+++ .hyper.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 18,
+    fontFamily: '"Dank Mono", monospace',
+    lineHeight: '1.5',
+    windowSize: [1080, 720],
+    padding: '10px',
+    shell: '/usr/local/bin/zsh',
+  },
+
+  plugins: ["hyper-night-owl", "hypercwd"],
+}
